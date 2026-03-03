b2e799543a8f75f262a94908ba7824841d8b93ad
Rik <161947286+RiquelmeLadislau@users.noreply.github.com>
2026-03-03 16:12:13 +0000

    projeto 1 e2

--- modulo1-fundamentos/01_calculo_imc/calculo_imc.rb
+++ modulo1-fundamentos/01_calculo_imc/calculo_imc.rb
@@ -1,1 +1,13 @@
 # crie sua solução aqui
+
+print("Digite sua altura")
+altura = gets.chomp.to_f
+puts altura.class
+
+
+print("Digite seu peso")
+peso = gets.chomp.to_f
+puts peso.class
+
+imc = peso / (altura*altura)
+print "Seu IMC é #{imc.round 2}."
--- modulo1-fundamentos/02_gerador_de_email/gerador_de_email.rb
+++ modulo1-fundamentos/02_gerador_de_email/gerador_de_email.rb
@@ -1,1 +1,12 @@
 # crie sua solução aqui
+
+print("Primeiro nome:")
+firstname = gets.chomp
+
+print("Segundo nome:")
+lastname = gets.chomp
+
+print("Company:")
+company = gets.chomp
+
+puts firstname + lastname +"@"+company+".com"
