7b7c4057dd25a944c73d4e4754439d5b2de7c94d
Rik <161947286+RiquelmeLadislau@users.noreply.github.com>
2026-03-04 11:45:13 +0000

    up proj3

--- modulo1-fundamentos/03_fibonacci/fibonacci.rb
+++ modulo1-fundamentos/03_fibonacci/fibonacci.rb
@@ -1,1 +1,13 @@
 # crie sua solução aqui
+print("digite até que sequência quer saber o fibonacci")
+fibonacci = gets.chomp.to_i
+
+fibo1 = 0
+fibo2 = 1
+
+fibonacci.times do
+    puts fibo1
+    soma = fibo1 + fibo2
+    fibo2 = fibo1
+    fibo1 = soma
+end
